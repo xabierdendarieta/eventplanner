68169201051ebd0fe568f48f5f15b4cbd0bde201
PaulVega <paul.vega.quishpe@gmail.com>
2020-05-30 18:59:52 +0200

    Update index.js
    Conexión con la base de datos correcta

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,15 @@
 // Utilizar funcionalidades del Ecmascript 6
 'use strict'
 // Cargamos el módulo de mongoose para poder conectarnos a MongoDB
-//var zmq = require('zmq');
-//const database = require('./database');
+var zmq = require('zeromq');
+//const database = require('./bbdd/database');
 
 // *Cargamos el fichero app.js con la configuración de Express
 var app = require('./app');
 
 // Connection
-//var ident = 'ident';
-//var socket = zmq.socket("dealer");
+var ident = 'ident';
+var socket = zmq.socket("dealer");
 
 // Creamos la variable PORT para indicar el puerto en el que va a funcionar el servidor
 var portDB = "1234";
@@ -18,10 +18,10 @@ var portApi = 3000;
 
 
 var host = "tcp://" + ipDB + ":" + portDB; 
-//socket.identity = ident;
+socket.identity = ident;
 
 // Connect to the server instance.
-/*socket.bind(host, function(){
+socket.bind(host, function(){
         module.exports = socket;
         // Cuando se realiza la conexión, lanzamos este mensaje por consola
         console.log("La conexión a la base de datos curso_mean_social se ha realizado correctamente");
@@ -30,13 +30,4 @@ var host = "tcp://" + ipDB + ":" + portDB;
         app.listen(portApi, () => {
             console.log("El servidor está inicializado en el puerto "+ portApi);
         });
-});*/
-
-//module.exports = socket;
-// Cuando se realiza la conexión, lanzamos este mensaje por consola
-console.log("La conexión a la base de datos curso_mean_social se ha realizado correctamente");
-
-// CREAR EL SERVIDOR WEB CON NODEJS
-app.listen(portApi, () => {
-    console.log("El servidor está inicializado en el puerto "+ portApi);
 });
